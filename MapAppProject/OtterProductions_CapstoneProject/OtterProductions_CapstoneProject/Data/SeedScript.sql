86b4c018f9c2417ff810533e412d0461977f28e3
cooperB117 <cooperjack23@gmail.com>
2023-02-08 13:49:49 -0800

    added connection string to connect the sql server db to web application, and scaffolded models and DbContext

--- MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Data/SeedScript.sql
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Data/SeedScript.sql
@@ -1,3 +1,12 @@
-INSERT INTO [Events](EventName, EventLocation, EventDiscreption)
+INSERT INTO [Events](OrganzationID, EventName, EventLocation, EventTypesID, EventDiscreption)
     VALUES
-    ('Food Bank Event 2023', 'Monmouth OR', 'Come forth those in need to be served a warm meal');
+    (1, 'Food Bank Event 2023', 'Monmouth OR', 1, 'Come forth those in need to be served a warm meal');
+
+INSERT INTO [Organzation](OrganzationLoginID, OrganzationDescription, OrganzationLocation)
+    VALUES
+    (1, 'Food Bank Organization', 'Monmouth OR');
+
+INSERT INTO [EventTypes](EventType)
+    VALUES
+    ('Food');
+
