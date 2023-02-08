c971a2c14f6262f52fc7115ab29afcbbd2c614b2
cooperB117 <cooperjack23@gmail.com>
2023-02-08 15:17:57 -0800

    Displayed event data from database onto event web page

--- MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Program.cs
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using OtterProductions_CapstoneProject.Models;
+
 namespace OtterProductions_CapstoneProject;
 
 public class Program
@@ -8,6 +11,9 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+           builder.Configuration.GetConnectionString("DefaultConnection")
+            ));
 
         var app = builder.Build();
 
