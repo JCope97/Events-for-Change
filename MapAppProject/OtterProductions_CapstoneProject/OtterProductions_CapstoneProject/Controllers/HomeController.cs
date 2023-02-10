70eb8477004d0d00bdc3c53c9c7032182f021733
cooperB117 <cooperjack23@gmail.com>
2023-02-10 06:44:32 -0800

    added authentication database which verifies for test purpose that there is connection from database to application and that user details is authenticated

--- MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/HomeController.cs
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OtterProductions_CapstoneProject.Areas.Identity.Data;
 using OtterProductions_CapstoneProject.Models;
 
 namespace OtterProductions_CapstoneProject.Controllers;
@@ -7,19 +10,33 @@ namespace OtterProductions_CapstoneProject.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
     public IActionResult Index()
     {
         return View();
     }
-
+    [Authorize]
     public IActionResult Privacy()
     {
+        return View();
+    }
+
+    public IActionResult DbConnection()
+    {
+        bool isAdmin = User.IsInRole("Admin");
+        bool isAuthenticated = User.Identity.IsAuthenticated;
+        string name = User.Identity.Name;
+        string authType = User.Identity.AuthenticationType;
+        string id = _userManager.GetUserId(User);
+
+        ViewBag.Message = $"User {name} is authenticated? {isAuthenticated} using type {authType} and is an Admin? {isAdmin} ID from Identity {id}";
         return View();
     }
 
