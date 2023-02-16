a8285535a240ac26184d73e73b56423330939d33
Jcope97 <jcope20@mail.wou.edu>
2023-02-16 15:53:41 -0800

    added map page and controler for map page, as well as the javascript page that runs the map and connected it all together and made sure that it runs

---
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/MapController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OtterProductions_CapstoneProject.Areas.Identity.Data;
+using OtterProductions_CapstoneProject.Data;
+using OtterProductions_CapstoneProject.Models;
+
+namespace OtterProductions_CapstoneProject.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Mappage()
+        {
+            return View();
+        }
+    }
+}
