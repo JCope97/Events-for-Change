68321b55c322b5e3e7adeb69bf50ab65b7453a9b
cooperB117 <cooperjack23@gmail.com>
2023-04-17 07:11:56 -0700

    fixed files in test project

---
+++ MapAppProject/OtterProductions_CapstoneProject/BDD_Tests_OtterProductions/PageObjects/OrganizationPageObject.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_Tests_OtterProductions.PageObjects
+{
+    public class OrganizationProfilePageObject : PageObject
+    {
+        public OrganizationProfilePageObject(IWebDriver webDriver) : base(webDriver)
+        {
+            _pageName = "Organization";
+        }
+        public IWebElement IsOrganizationCheckBox => _webDriver.FindElement(By.Name("IsOrganization"));
+        public IWebElement fileUploadTextBox => _webDriver.FindElement(By.Name("fileUpload"));
+        public IWebElement EmailTextBox => _webDriver.FindElement(By.Name("Email"));
+        public IWebElement OrganizationNameTextBox => _webDriver.FindElement(By.Name("OrganizationName"));
+        public IWebElement OrganizationDescriptionTextBox => _webDriver.FindElement(By.Name("OrganizationDescription"));
+        public IWebElement OrganizationLocationTextBox => _webDriver.FindElement(By.Name("OrganizationLocation"));
+        public IWebElement AddressTextBox => _webDriver.FindElement(By.Name("Address"));
+        public IWebElement PhoneNumbernTextBox => _webDriver.FindElement(By.Name("PhoneNumber"));
+        public IWebElement PasswordTextBox => _webDriver.FindElement(By.Name("OrganizationLoginId"));
+        public IWebElement ConfirmPasswordTextBox => _webDriver.FindElement(By.Name("ConfirmPassword"));
+        public IWebElement CreateAccounBoxButton => _webDriver.FindElement(By.Id("registerSubmit"));
+    }
+}
