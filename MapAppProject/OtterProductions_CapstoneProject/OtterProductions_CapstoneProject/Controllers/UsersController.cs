f285fd791fc0a2cb538f1cd6430b32100be72893
AntoniaMMorales <brand@DESKTOP-ETMCMKC.(none)>
2023-05-19 16:26:03 -0700

    Added editing phone number capabilities; working on editing email

--- MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/UsersController.cs
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/UsersController.cs
@@ -44,206 +44,34 @@ namespace OtterProductions_CapstoneProject.Controllers
 
         }
 
-        //https://www.geeksforgeeks.org/basic-crud-create-read-update-delete-in-asp-net-mvc-using-c-sharp-and-entity-framework/
-        // To specify that this will be
-        // invoked when post method is called
+        // Used https://www.geeksforgeeks.org/basic-crud-create-read-update-delete-in-asp-net-mvc-using-c-sharp-and-entity-framework/
+        // as reference for post method 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> EditInfo(int Userid, ApplicationUser user)
+        public async Task<ActionResult> EditInfo(ApplicationUser user)
         {
+            var userId = _userManager.GetUserId(User);
+            var userInfo = _userManager.GetUserAsync(User);
+            var data2 = _context.MapAppUsers.Where(x => x.AspnetIdentityId == userId).SingleOrDefault();
 
+            var tokenPhone = await _userManager.GenerateChangePhoneNumberTokenAsync(await userInfo, user.PhoneNumber);
+            var tokenEmail = await _userManager.GenerateChangeEmailTokenAsync(await userInfo, user.Email);
 
-            // Use of lambda expression to access
-            // particular record from a database
-            //var data = _context.MapAppUsers.FirstOrDefault(x => x.Id == Userid);
-            var data2 = _userManager.Users.FirstOrDefault(x => x.Id == Userid.ToString());
-            Console.WriteLine(5 + 5);
-            Console.WriteLine("Hello");
-            Console.WriteLine(data2);
-            var token = await _userManager.GenerateChangePhoneNumberTokenAsync(user, user.Email);
             // Checking if any such record exist
             if (data2 != null)
             {
-                // _userManager.ChangePhoneNumberAsync(data2, user.PhoneNumber, token);
-                 _userManager.ChangePhoneNumberAsync(user, data2.PhoneNumber, token);
+                data2.PhoneNumber = user.PhoneNumber;
+                data2.Email = user.Email;
+               
+                var updateNum = _userManager.ChangePhoneNumberAsync(await userInfo, user.PhoneNumber, tokenPhone);
+                var updateEmail = _userManager.ChangeEmailAsync(await userInfo, user.Email,tokenEmail);
 
-                //_userManager.ChangePhoneNumberAsync(user, user.PhoneNumber, data2.PhoneNumber);
-
-                //data.FirstName = user.FirstName;
-                //data.LastName = user.LastName;
-                //data.Email = user.Email;
-                //data.PhoneNumber = user.PhoneNumber;
-                //data2.FirstName = user.FirstName;
-                //data2.LastName = user.LastName;
-                //data2.Email = user.Email;
-                //data2.PhoneNumber = user.PhoneNumber;
-
-                //_userManager.UpdateAsync(data2);
-                //_context.SaveChanges();
-                //_authenticationDbContext.SaveChanges();
-                //_userManager.SetPhoneNumberAsync(user, )
-
-
-                // It will redirect
-                return RedirectToAction("/Home/Index");
+                var change = _context.SaveChanges();
+ 
+                return RedirectToAction("Index", "/Home");
             }
             else
                 return View();
-
-        }
-
-        //public IActionResult EditInfo()
-        //{
-        //    return View();
-
-        //}
-
-
-        //[HttpGet]
-        //public async Task<IActionResult> EditUser(string id)
-        //{
-        //    var userId = _userManager.GetUserId(User);
-
-
-        //    if (userId == null)
-        //    {
-        //        return View();
-        //    }
-        //    var edit = _context.MapAppUsers.Where(x => x.AspnetIdentityId == userId).FirstOrDefault();
-        //    if (edit == null)
-        //    {
-        //        return NotFound();
-
-        //    }
-        //    //toDo: We need to user the Autompper
-        //    var editUser = new UserViewModel()
-        //    {
-        //        Email = edit.Email,
-        //        FirstName = edit.FirstName,
-        //        LastName = edit.LastName,
-        //        PhoneNumber = edit.PhoneNumber,
-        //        OldEmail = edit.Email
-        //    };
-        //    return View(editUser);
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> EditUser(UserViewModel user)
-        //{
-        //    var userId = _userManager.GetUserId(User);
-        //    if (userId == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            var editUser = _context.MapAppUsers.Where(x => x.AspnetIdentityId == userId).FirstOrDefault();
-
-        //            if (editUser == null)
-        //                return NotFound();
-
-        //            var hasEmailChanged = editUser.Email != user.Email;
-
-
-        //            editUser.Email = user.Email;
-        //            editUser.FirstName = user.FirstName;
-        //            editUser.LastName = user.LastName;
-        //            editUser.PhoneNumber = user.PhoneNumber;
-
-        //            _context.Update(editUser);
-        //            await _context.SaveChangesAsync();
-
-        //            var newUser = _authenticationDbContext.Users.FirstOrDefault(x => x.Email == user.OldEmail);
-        //            if (newUser != null)
-        //            {
-        //                newUser.PhoneNumber = user.PhoneNumber;
-        //                newUser.Email = user.Email;
-        //                newUser.NormalizedEmail = user.Email.ToUpper();
-        //                newUser.NormalizedUserName = user.Email.ToUpper();
-        //                _authenticationDbContext.Users.Update(newUser);
-        //                await _authenticationDbContext.SaveChangesAsync();
-        //            }
-
-        //            if (hasEmailChanged)
-        //            {
-        //                await _signInManager.SignOutAsync();
-        //            }
-
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!UserExists(user.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //    }
-        //    return View(user);
-        //}
-
-
-        //// GET: Organization/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null || _context.MapAppUsers == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var organization = await _context.MapAppUsers.FindAsync(id);
-        //    if (organization == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(organization);
-        //}
-
-        //// POST: Organization/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,AspnetIdentityId,Email,FirstName,LastName,PhoneNumber")] MapAppUser user)
-        //{
-        //    if (id != user.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(user);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!UserExists(user.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(user);
-        //}
-
-        private bool UserExists(int id)
-        {
-            return (_context.MapAppUsers?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
