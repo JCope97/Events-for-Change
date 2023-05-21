bf61ea4f72a93becdd1f3096e2c03c2f0ae4d73f
AntoniaMMorales <brand@DESKTOP-ETMCMKC.(none)>
2023-05-20 19:06:40 -0700

    Added option to change email

--- MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/UsersController.cs
+++ MapAppProject/OtterProductions_CapstoneProject/OtterProductions_CapstoneProject/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using OtterProductions_CapstoneProject.Areas.Identity.Data;
 using OtterProductions_CapstoneProject.Data;
 using OtterProductions_CapstoneProject.Models;
@@ -31,14 +32,11 @@ namespace OtterProductions_CapstoneProject.Controllers
         }
 
         //https://www.geeksforgeeks.org/basic-crud-create-read-update-delete-in-asp-net-mvc-using-c-sharp-and-entity-framework/
-        public ActionResult EditInfo(int Userid)
+        public ActionResult EditInfo(string id)
         {
             var userId = _userManager.GetUserId(User);
 
             var data = _context.MapAppUsers.Where(x => x.AspnetIdentityId == userId).SingleOrDefault();
-            Console.WriteLine(5 + 5);
-            Console.WriteLine("Hello");
-            Console.WriteLine(data);
 
             return View(data);
 
@@ -48,26 +46,55 @@ namespace OtterProductions_CapstoneProject.Controllers
         // as reference for post method 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> EditInfo(ApplicationUser user)
+        public async Task<ActionResult> EditInfo(string id, string email, string phoneNumber)
         {
             var userId = _userManager.GetUserId(User);
+            ApplicationUser user = await _userManager.FindByIdAsync(userId);
+            //These are getting the user's info from the dbs
+            
             var userInfo = _userManager.GetUserAsync(User);
             var data2 = _context.MapAppUsers.Where(x => x.AspnetIdentityId == userId).SingleOrDefault();
 
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                {
+                    user.Email = email;
+                    user.UserName = email;
+                    user.NormalizedEmail = email.ToUpper();
+                    user.NormalizedUserName = email.ToUpper();
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email is empty");
+                }
+                if (!string.IsNullOrEmpty(phoneNumber))
+                {
+                    user.PhoneNumber = phoneNumber;
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Phone number is empty");
+                }
+            }
+            //creating tokens to check when changing identity db info
             var tokenPhone = await _userManager.GenerateChangePhoneNumberTokenAsync(await userInfo, user.PhoneNumber);
-            var tokenEmail = await _userManager.GenerateChangeEmailTokenAsync(await userInfo, user.Email);
-
+           // var tokenEmail = await _userManager.GenerateChangeEmailTokenAsync(await userInfo, user.Email);
             // Checking if any such record exist
             if (data2 != null)
             {
-                data2.PhoneNumber = user.PhoneNumber;
-                data2.Email = user.Email;
+                //Updating application db
+                data2.PhoneNumber = phoneNumber;
+                data2.Email = email;
+                
+                //user.UserName = user.Email;
                
-                var updateNum = _userManager.ChangePhoneNumberAsync(await userInfo, user.PhoneNumber, tokenPhone);
-                var updateEmail = _userManager.ChangeEmailAsync(await userInfo, user.Email,tokenEmail);
-
+                //updating identity db
+                var updateNum = await _userManager.ChangePhoneNumberAsync(await userInfo, user.PhoneNumber, tokenPhone);
+                //var updateEmail = await _userManager.ChangeEmailAsync(await userInfo, user.Email, tokenEmail);
                 var change = _context.SaveChanges();
- 
+
+                
                 return RedirectToAction("Index", "/Home");
             }
             else
